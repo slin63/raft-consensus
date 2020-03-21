b8981c0efc6f402786cf686a2cb0bf85a1073199
Shean Lin <shean.lin2018@gmail.com>
2020-03-21 15:11:25 -0500

    move digest entry / commit functions around to a more appropriate spot

--- internal/node/apply.go
+++ internal/node/apply.go
@@ -3,6 +3,8 @@ package node
 import (
     "fmt"
     "log"
+    "sync"
+    "time"
 
     "github.com/slin63/raft-consensus/internal/config"
     "github.com/slin63/raft-consensus/internal/spec"
@@ -39,7 +41,6 @@ func applyCommits(idx int) *responses.Result {
     current := start
     config.LogIf(fmt.Sprintf("[APPLY]: Applying %d entries", start-end), config.C.LogDigestCommits)
     for _, entry := range raft.Log[start:end] {
-
         var result responses.Result
         if err := (*client).Call("Filesystem.Execute", entry, &result); err != nil {
             log.Fatal(err)
@@ -63,4 +64,67 @@ func applyCommits(idx int) *responses.Result {
 
     raft.CommitIndex = idx
     return r
+}
+
+func digestCommits() {
+    // Digest commits in order.
+    //   - Applies that fail crash the server
+    //   - Entries that are not successful are sent downstream
+    for commit := range commits {
+        config.LogIf(fmt.Sprintf("[APPLY]: Applying index %d", commit.Idx), config.C.LogDigestCommits)
+        r := applyCommits(commit.Idx)
+        config.LogIf(fmt.Sprintf("[APPLY]: Successfully applied index %d", commit.Idx), config.C.LogDigestCommits)
+
+        select {
+        case commit.C <- r:
+        case <-time.After(time.Second * time.Duration(config.C.RPCTimeout)):
+            config.LogIf(fmt.Sprintf("[DIGESTCOMMITS]: Response channel blocked"), config.C.LogDigestCommits)
+        }
+    }
+}
+
+func digestEntries() {
+    // Digest client entries in order
+    for entry := range entries {
+        var once sync.Once
+        // Add new entry to own log
+        spec.RaftRWMutex.Lock()
+        config.LogIf(fmt.Sprintf("[DIGESTENTRIES]: Processing [%s]", entry.D), config.C.LogDigestEntriesVerbose)
+        idx := raft.AppendEntry(entry.D)
+        config.LogIf(fmt.Sprintf("[DIGESTENTRIES]: New log %s", raft.Log), config.C.LogDigestEntriesVerbose)
+        spec.RaftRWMutex.Unlock()
+
+        rch := make(chan *responses.Result)
+        rcount := 0
+
+        // Dispatch AppendEntries to follower nodes
+        spec.SelfRWMutex.RLock()
+        quorum := spec.GetQuorum(&self)
+        remaining := len(self.MemberMap) - 1
+        for PID := range self.MemberMap {
+            if PID != self.PID {
+                go func(PID int, remaining *int) {
+                    r := appendEntriesUntilSuccess(raft, PID)
+                    r.Index = idx
+                    rch <- r
+                    if *remaining -= 1; *remaining == 0 {
+                        close(rch)
+                    }
+                }(PID, &remaining)
+            }
+        }
+        spec.SelfRWMutex.RUnlock()
+
+        // Parse responses from servers and notify RPC about safely committed entries.
+        for r := range rch {
+            if !r.Success {
+                panic("appendEntriesUntilSuccess should never fail")
+            }
+            rcount += 1
+            if rcount >= quorum {
+                config.LogIf(fmt.Sprintf("[DIGESTENTRIES] QUOROM received (%d/%d) [entry=%s]", rcount, quorum, tr(entry.D, 20)), config.C.LogDigestEntries)
+                once.Do(func() { entry.C <- r })
+            }
+        }
+    }
 }
